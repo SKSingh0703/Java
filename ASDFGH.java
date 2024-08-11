2122ad21875096ba084f541bdf4960cd981df823
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-08-11 07:58:45 +0530

    ok

--- ASDFGH.java
+++ ASDFGH.java
@@ -77,4 +77,70 @@ public class ASDFGH {
         }
         return ans;
     }
+    public int numMagicSquaresInside(int[][] grid) {
+        if (grid.length < 3 || grid[0].length < 3) {
+            return 0;
+        }
+        int rows=grid.length;
+        int cols=grid[0].length;
+        int count=0;
+        for(int i=0;i<rows-3;i++){
+            for(int j=0;j<cols-3;j++){
+                if (isSafe(i,j,grid)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private boolean isSafe(int row,int col,int grid[][]){
+        boolean distinct[]=new boolean[16];
+        
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                distinct[grid[i][j]]=true;
+            }
+        }
+        for(int i=1;i<=9;i++){
+            if (!distinct[i]) {
+                return false;
+            }
+        }
+
+        int colSum=0;
+        for(int i=row;i<row+3;i++){
+            colSum+=grid[i][col];
+        }
+
+        for(int i=row;i<row+3;i++){
+            int currSum=0;
+            for(int j=col;j<col+3;j++){
+                currSum+=grid[i][j];
+            }
+            if (currSum!=colSum) {
+                return false;
+            }
+        }
+
+        for(int i=col;i<col+3;i++){
+            int currSum=0;
+            for(int j=row;j<row+3;j++){
+                currSum+=grid[j][i];
+            }
+            if (currSum!=colSum) {
+                return false;
+            }
+        }
+
+        int diagnolSum=grid[row][col]+grid[row+1][col+1]+grid[row+2][col+2];
+        if (colSum!=diagnolSum) {
+            return false;
+        }
+        int diagnolSumTwo=grid[row][col+2]+grid[row+1][col+1]+grid[row+2][col];
+        if (colSum!=diagnolSumTwo) {
+            return false;
+        }
+
+        return true;
+    }
 }
