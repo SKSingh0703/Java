f7e1974ef33ec4e4a92d245addf8c33e6bbdb896
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-09-12 13:56:56 +0530

    ii

--- IEEEcontest.java
+++ IEEEcontest.java
@@ -239,6 +239,26 @@ class Solution {
         }
         return ans;
     }
+    public int countConsistentStrings(String allowed, String[] words) {
+        HashSet<Character> hs=new HashSet<>();
+        for(int i=0;i<allowed.length();i++){
+            hs.add(allowed.charAt(i));
+        }
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            boolean flag=true;
+            for(int j=0;j<words[i].length();j++){
+                if (!hs.contains(words[i].charAt(j))) {
+                    flag=false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
