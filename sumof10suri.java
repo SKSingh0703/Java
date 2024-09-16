9aaec014cd047cb524999504669c00ffa3d6fa46
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-09-16 12:00:49 +0530

    ff

--- sumof10suri.java
+++ sumof10suri.java
@@ -8,8 +8,21 @@ public class sumof10suri {
             }
         }    
     }
-    public int findTheLongestSubstring(String s) {
+    public int findMinDifference(List<String> timePoints) {
         
+    }
+    public int findTheLongestSubstring(String s) {
+        int count[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+                count[ch-'a']++;
+            }
+        }
+        if (count[0]%2!=0) {
+            
+        }s.indexOf(, 0)
+
     }
     public static void main(String[] args) {
         
