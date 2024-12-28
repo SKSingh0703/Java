970833d4832c939d193c0f9952063a5509916fc9
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-12-28 19:08:09 +0530

    mmmmmmmmm

---
+++ uyboiomp.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class uyboiomp {
+    public static void main(String[] args) {
+        ArrayList<Integer> curr=new ArrayList<>();
+
+        int ans[] = curr.toArray()
+    }
+}
