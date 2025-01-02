567168aae4d6fcd656679ab685db3f45ffeb3e86
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-01-02 12:13:47 +0530

    fff

--- uyboiomp.java
+++ uyboiomp.java
@@ -6,4 +6,25 @@ public class uyboiomp {
 
         int ans[] = curr.toArray()
     }
+
+    class Solution {
+        public int[] vowelStrings(String[] words, int[][] queries) {
+            int n = words.length;
+            int m = queries.length;
+            int prefix[] = new int[n+1];
+            prefix[0]=0;
+            
+            for(int i=0;i<n;i++){
+                prefix[i+1] = prefix[i] + words[i].charAt(0)==words[i].charAt(words[i].length()-1)?1:0;
+            }
+    
+            int ans[] = new int[m];
+            for(int i=0;i<m;i++){
+                int q[]=queries[i];
+                ans[i]=prefix[q[1]+1]-prefix[q[0]];
+            }
+    
+            return ans;
+        }
+    }
 }
