91edd8b35ccd894338b8d49e9c2e99bc2f75505f
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-01-13 14:52:13 +0530

    vvv

--- abc.java
+++ abc.java
@@ -6,55 +6,28 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class abc {
-    public static void main(String[] args) {
-        try {
-            // Create the URL object
-            URL url = new URL("https://leetcode.com/graphql/");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0"); // Mimic a browser
-            conn.setDoOutput(true);
-
-            // GraphQL query to fetch questions
-            String query = """
-            {
-                "query": "query { problemsetQuestionList(categorySlug: \"\", limit: 10) { questions { titleSlug } } }"
-            }
-            """;
-
-            // Send request
-            try (OutputStream os = conn.getOutputStream()) {
-                os.write(query.getBytes());
-            }
-
-            // Check response code
-            int responseCode = conn.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Read response
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                    String responseLine;
-                    while ((responseLine = br.readLine()) != null) {
-                        System.out.println(responseLine);
-                    }
-                }
-            } else {
-                // Read error response
-                InputStream errorStream = conn.getErrorStream();
-                if (errorStream != null) {
-                    try (BufferedReader br = new BufferedReader(new InputStreamReader(errorStream))) {
-                        String responseLine;
-                        while ((responseLine = br.readLine()) != null) {
-                            System.out.println("Error Response: " + responseLine);
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static int solve(int[] a, int b) {
+        int n = a.length;
+        int prefix[] = new int[n];
+        prefix[0]=a[0];
+        
+        for(int i=1;i<n;i++){
+            prefix[i]=(prefix[i-1]^a[i]);
         }
+        int count =0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int xor = prefix[i]^prefix[j];
+                if(i==j) xor=a[i];
+                System.out.println(i+"  "+j+"  "+xor);
+                if(xor==b) count++;
+            }
+        }
+        
+        return count;
+    }
+    public static void main(String[] args) {
     }
 }
 
