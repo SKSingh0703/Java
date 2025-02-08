9816174bb4046dd2c3605dcd27b7e7d027693811
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-02-08 15:28:47 +0530

    medium : design-a-number-container-system

--- abc.java
+++ abc.java
@@ -6,6 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class abc {
     public static int solve(int[] a, int b) {
@@ -103,6 +104,59 @@ class Solution {
         return ans;
     }
 }
+class NumberContainers {
+
+    public NumberContainers() {
+        numberToIndices = new HashMap<>();
+        indexToNumbers = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        // Update index to number mapping
+        indexToNumbers.put(index, number);
+
+        // Add index to the min heap for this number
+        numberToIndices
+            .computeIfAbsent(number, k -> new PriorityQueue<>())
+            .add(index);
+    }
+
+    public int find(int number) {
+        // If number doesn't exist in our map
+        if (!numberToIndices.containsKey(number)) {
+            return -1;
+        }
+
+        // Get min heap for this number
+        PriorityQueue<Integer> minHeap = numberToIndices.get(number);
+
+        // Keep checking top element until we find valid index
+        while (!minHeap.isEmpty()) {
+            int index = minHeap.peek();
+
+            // If index still maps to our target number, return it
+            if (indexToNumbers.get(index) == number) {
+                return index;
+            }
+
+            // Otherwise remove this stale index
+            minHeap.poll();
+        }
+
+        return -1;
+    }
+
+    // Map to store number -> min heap of indices
+    private Map<Integer, PriorityQueue<Integer>> numberToIndices;
+    // Map to store index -> number
+    private Map<Integer, Integer> indexToNumbers;
+}
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
     class Solution {
         public int maxAscendingSum(int[] nums) {
             int sum = nums[0];
