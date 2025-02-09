ba7d77920b90330878afa1d9954a5f0d0aea2b84
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-02-09 20:00:26 +0530

    medium : count-number-of-bad-pairs

--- abc.java
+++ abc.java
@@ -157,6 +157,28 @@ class NumberContainers {
  * obj.change(index,number);
  * int param_2 = obj.find(number);
  */
+class Solution {
+
+    public long countBadPairs(int[] nums) {
+        long badPairs = 0;
+        Map<Integer, Integer> diffCount = new HashMap<>();
+
+        for (int pos = 0; pos < nums.length; pos++) {
+            int diff = pos - nums[pos];
+
+            // Count of previous positions with same difference
+            int goodPairsCount = diffCount.getOrDefault(diff, 0);
+
+            // Total possible pairs minus good pairs = bad pairs
+            badPairs += pos - goodPairsCount;
+
+            // Update count of positions with this difference
+            diffCount.put(diff, goodPairsCount + 1);
+        }
+
+        return badPairs;
+    }
+}
     class Solution {
         public int maxAscendingSum(int[] nums) {
             int sum = nums[0];
