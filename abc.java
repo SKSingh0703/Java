4907ba70c3eb4a04e1dbd60323b85f4e095a8bfc
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-02-26 22:26:30 +0530

    maximum-absolute-sum-of-any-subarray

--- abc.java
+++ abc.java
@@ -736,6 +736,22 @@ class Solution {
         return count;
     }
 }
+class Solution {
+
+    public int maxAbsoluteSum(int[] nums) {
+        int minPrefixSum = 0, maxPrefixSum = 0;
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+
+            minPrefixSum = Math.min(minPrefixSum, prefixSum);
+            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+        }
+
+        return maxPrefixSum - minPrefixSum;
+    }
+}
 
     class Solution {
         public int maxAscendingSum(int[] nums) {
