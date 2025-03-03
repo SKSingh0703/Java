e9f8924cec79b3acded3b2537fa1e07befcfa5a0
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-03-03 16:39:43 +0530

    partition-array-according-to-given-pivot

--- abc.java
+++ abc.java
@@ -888,7 +888,32 @@ class Solution {
         return res;
     }
 }
+class Solution {
 
+    public int[] pivotArray(int[] nums, int pivot) {
+        LinkedList<Integer> less = new LinkedList<>();
+        LinkedList<Integer> equal = new LinkedList<>();
+        LinkedList<Integer> greater = new LinkedList<>();
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num > pivot) {
+                greater.add(num);
+            } else {
+                equal.add(num);
+            }
+        }
+        less.addAll(equal);
+        less.addAll(greater);
+
+        int i = 0;
+        int[] ans = new int[nums.length];
+        for (int num : less) {
+            ans[i++] = num;
+        }
+        return ans;
+    }
+}
     class Solution {
         public int maxAscendingSum(int[] nums) {
             int sum = nums[0];
