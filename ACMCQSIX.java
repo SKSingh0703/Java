0336df5b2b2ff0cb495894558ba98d47d9d115c8
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-03-19 14:45:40 +0530

    minimum-operations-to-make-binary-array-elements-equal-to-one-i

--- ACMCQSIX.java
+++ ACMCQSIX.java
@@ -523,7 +523,24 @@ class Solution {
         return maxLength;
     }
 }
-
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for(int i = 0;i < nums.length - 2;i++){
+            if(nums[i] == 0){
+                nums[i] = 1 - nums[i];
+                nums[i + 1] = 1 - nums[i + 1];
+                nums[i + 2] = 1 - nums[i + 2];
+                count += 1;
+            }
+        }
+        if(nums[nums.length - 1] == 0 || nums[nums.length - 2] == 0){
+            return -1;
+        }
+        
+        return count;
+    }
+}
 class Solution {
     public int maximumCount(int[] nums) {
         int p = 0;
