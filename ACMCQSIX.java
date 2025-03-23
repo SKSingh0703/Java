7e5ad066e55f37869bfc8252e815dcb7488b880c
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-03-23 15:49:25 +0530

    number-of-ways-to-arrive-at-destination

--- ACMCQSIX.java
+++ ACMCQSIX.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
@@ -671,6 +674,64 @@ class Solution {
         }
 
         return createdRecipes;
+    }
+}
+class Solution {
+
+    public int countPaths(int n, int[][] roads) {
+        final int MOD = 1_000_000_007;
+
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] road : roads) {
+            int startNode = road[0], endNode = road[1], travelTime = road[2];
+            graph.get(startNode).add(new int[] { endNode, travelTime });
+            graph.get(endNode).add(new int[] { startNode, travelTime });
+        }
+
+        PriorityQueue<long[]> minHeap = new PriorityQueue<>(
+            Comparator.comparingLong(a -> a[0])
+        );
+
+        long[] shortestTime = new long[n];
+        Arrays.fill(shortestTime, Long.MAX_VALUE);
+        int[] pathCount = new int[n];
+
+        shortestTime[0] = 0; 
+        pathCount[0] = 1; 
+
+        minHeap.offer(new long[] { 0, 0 }); 
+
+        while (!minHeap.isEmpty()) {
+            long[] top = minHeap.poll();
+            long currTime = top[0]; 
+            int currNode = (int) top[1];
+
+            // Skip outdated distances
+            if (currTime > shortestTime[currNode]) {
+                continue;
+            }
+
+            for (int[] neighbor : graph.get(currNode)) {
+                int neighborNode = neighbor[0], roadTime = neighbor[1];
+
+                if (currTime + roadTime < shortestTime[neighborNode]) {
+                    shortestTime[neighborNode] = currTime + roadTime;
+                    pathCount[neighborNode] = pathCount[currNode];
+                    minHeap.offer(
+                        new long[] { shortestTime[neighborNode], neighborNode }
+                    );
+                }
+                else if (currTime + roadTime == shortestTime[neighborNode]) {
+                    pathCount[neighborNode] =
+                        (pathCount[neighborNode] + pathCount[currNode]) % MOD;
+                }
+            }
+        }
+
+        return pathCount[n - 1];
     }
 }
 class Solution {
