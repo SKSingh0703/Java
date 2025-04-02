8de61d95aa1e522156b254a558e553f9c26a7b35
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-04-02 09:49:45 +0530

    maximum-value-of-an-ordered-triplet-i

--- aaaaaa.java
+++ aaaaaa.java
@@ -225,7 +225,19 @@ class UnionFind {
             return dp[idx] = Math.max(ch1,ch2);
         }
     }
+    class Solution {
 
+        public long maximumTripletValue(int[] nums) {
+            int n = nums.length;
+            long res = 0, imax = 0, dmax = 0;
+            for (int k = 0; k < n; k++) {
+                res = Math.max(res, dmax * nums[k]);
+                dmax = Math.max(dmax, imax - nums[k]);
+                imax = Math.max(imax, nums[k]);
+            }
+            return res;
+        }
+    }
     // Get the size of the component containing a given node
     public int getSize(int node) {
         return size[find(node)];
