1ff3b6362752daaf40917b9a2d620945d625eddc
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-04-24 18:20:11 +0530

    count-complete-subarrays-in-an-array

--- aaaaaa.java
+++ aaaaaa.java
@@ -770,4 +770,24 @@ class Solution {
         return count;
     }
 }
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        Set<Integer> unique = new HashSet<>();
+        for (int num : nums) unique.add(num);
+        int totalDistinct = unique.size(), count = 0, n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i; j < n; j++) {
+                seen.add(nums[j]);
+                if (seen.size() == totalDistinct) {
+                    count += n - j;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
 }
