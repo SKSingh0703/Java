1cba1d1368d43e3fb9b0173a68097f55b00cd772
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-04-27 19:45:21 +0530

    count-subarrays-of-length-three-with-a-condition

--- aaaaaa.java
+++ aaaaaa.java
@@ -828,5 +828,23 @@ class Solution {
         }
         return count;
     }
+    class Solution {
+        public int countSubarrays(int[] nums) {
+            int count = 0;
+    
+            for (int i = 0; i < nums.length - 2; i++) {
+                int first = nums[i];
+                int second = nums[i + 1];
+                int third = nums[i + 2];
+    
+                if (first + third == second / 2.0) {
+                    count++;
+                }
+            }
+    
+            return count;
+        }
+    }
+    
 }
 }
