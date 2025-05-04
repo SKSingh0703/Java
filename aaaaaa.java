398761d8ba03e2b8b1af5b1d22dbca9b3b84b91d
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-05-04 14:18:46 +0530

    number-of-equivalent-domino-pairs

--- aaaaaa.java
+++ aaaaaa.java
@@ -990,4 +990,19 @@ class Solution {
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
+class Solution {
+    public int numEquivDominoPairs(int[][] d) {
+        int n = d.length;
+        int count = 0;
+        for(int i =0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if((d[i][0]==d[j][0] && d[i][1]==d[j][1]) || (d[i][0]==d[j][1] && d[i][1]==d[j][0])){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
 }
