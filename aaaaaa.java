0f9b1d24aa6ecbd287a6ef1e5f802da758d15d24
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-05-06 13:56:28 +0530

    build-array-from-permutation

--- aaaaaa.java
+++ aaaaaa.java
@@ -1042,4 +1042,16 @@ class Solution {
         return (int)ans;
     }
 }
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        for(int i = 0;i<n;i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
 }
