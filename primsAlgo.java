89edd25be25ab8798ffbb8144b1ade6312030549
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-05-22 12:45:07 +0530

    zero-transformation-array-iii

--- primsAlgo.java
+++ primsAlgo.java
@@ -147,4 +147,31 @@ class Solution {
         return true;
     }
 }
+class Solution {
+    public int maxRemoval(int[] nums, int[][] queries) {
+        Arrays.sort(queries, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+        int m = queries.length;
+        int n = nums.length;
+        int[] end = new int[n + 1];
+        int val = 0;
+        int j = 0;
+        
+        for(int i = 0; i < n; ++i) {
+            val -= end[i];
+            while(j < m && queries[j][0] <= i) {
+                pq.add(queries[j][1]);
+                j++;
+            }
+
+            while(val < nums[i]) {
+                if(pq.isEmpty() || pq.peek() < i) return -1;
+                end[pq.poll() + 1]++;
+                val++;
+            }
+        }
+
+        return pq.size();
+    }
+}
  
