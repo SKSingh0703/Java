8f799b26dbf6b893637a94853d1e43d4a7a9220d
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-05-25 10:37:41 +0530

    longest-palindrome-by-concatenating-two-letter-words

--- primsAlgo.java
+++ primsAlgo.java
@@ -187,7 +187,24 @@ class Solution {
     }
 }
 class Solution {
+    public int longestPalindrome(String[] words) {
+        int ans=0;
+        int arr[][]=new int[26][26];
+        for(String s:words){
+            int a=s.charAt(0)-'a',b=s.charAt(1)-'a';
+            if(arr[b][a]!=0){
+                arr[b][a]--;
+                ans+=4;
+            }
+            else arr[a][b]++;
+        }
+        for(int i=0;i<26;i++) if(arr[i][i]!=0){ans+=2; break;}
+        return ans;
+    }
+}
+class Solution {
     public int maxRemoval(int[] nums, int[][] queries) {
+        
         Arrays.sort(queries, (a, b) -> a[0] - b[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
         int m = queries.length;
