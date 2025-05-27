716f6f2ce356220ff98e437f0fe3bf84c45777b3
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-05-27 15:18:53 +0530

    divisible-and-non-divisible-sums-difference

--- primsAlgo.java
+++ primsAlgo.java
@@ -251,7 +251,21 @@ class Solution {
         return processed == n ? maxColorCount : -1;
     }
 }
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
 
+        for(int i = 1;i<=n;i++){
+            if(i%m==0){
+                num2+=i;
+            }
+            else num1+=i;
+        }
+
+        return num1-num2;
+    }
+}
 class Solution {
     public int maxRemoval(int[] nums, int[][] queries) {
         
