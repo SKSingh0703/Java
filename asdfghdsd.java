1848da6124af682fc35872a42bb630951c95e414
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-06-10 11:02:31 +0530

    maximum-difference-between-even-and-odd-frequency-i

--- asdfghdsd.java
+++ asdfghdsd.java
@@ -299,3 +299,22 @@ class Solution {
         return steps;
     }
 }
+class Solution {
+    public int maxDifference(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int maxDiff = Integer.MIN_VALUE;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0 || freq[i] % 2 == 0) continue;
+            for (int j = 0; j < 26; j++) {
+                if (freq[j] == 0 || freq[j] % 2 != 0) continue;
+                maxDiff = Math.max(maxDiff, freq[i] - freq[j]);
+            }
+        }
+
+        return maxDiff == Integer.MIN_VALUE ? 0 : maxDiff;
+    }
+}
