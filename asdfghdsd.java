c65c8a7a6cff6cfb37d5f098a1988bd894c84cb5
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-06-13 12:31:18 +0530

    minimize-the-maximum-difference-of-pairs

--- asdfghdsd.java
+++ asdfghdsd.java
@@ -367,4 +367,36 @@ class Solution {
     }
 }
 
-sdsd
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int low = 0, high = nums[nums.length - 1] - nums[0];
+        int answer = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canFormPairs(nums, p, mid)) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean canFormPairs(int[] nums, int p, int maxDiff) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i + 1] - nums[i] <= maxDiff) {
+                count++;
+                i += 2; 
+            } else {
+                i++;
+            }
+        }
+        return count >= p;
+    }
+}
