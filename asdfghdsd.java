238c7c157c99e606b1c53c58f780fbb511cab7bb
Sachin Kumar <sachinkumar9031735255@gmail.com>
2025-07-01 09:23:29 +0530

    find-the-original-typed-string-i

--- asdfghdsd.java
+++ asdfghdsd.java
@@ -826,4 +826,16 @@ public class Solution {
         return maxLength;
     }
 }
+class Solution {
+    public int possibleStringCount(String word) {
+        long ans = 1;                               // flawless string
+        for (int i = 0, n = word.length(); i < n; ) {
+            int j = i;
+            while (j < n && word.charAt(j) == word.charAt(i)) j++;
+            ans += (j - i - 1);                     // L‑1 extras
+            i = j;
+        }
+        return (int) ans;
+    }
+}
 
