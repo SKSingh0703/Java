8c5e85693c90aadd409298322eac4fd8ca6b0d7a
Sachin Kumar <sachinkumar9031735255@gmail.com>
2026-05-16 17:09:08 +0530

    Day 8

--- aRecursion.java
+++ aRecursion.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class aRecursion {
-    public static double pow(double x , int n){
-        if(n==1) return x;
+    public static int maxSubArray(int[] nums) {
+        int currmax = Integer.MIN_VALUE , max = Integer.MIN_VALUE , n = nums.length;
 
-        double hp = pow(x, n/2);
+        List<Integer> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+ 
+        for(int i = 0;i<n;i++){
+            currmax = currmax+nums[i];
+            if(currmax<0){
+                currmax = 0;
+                curr = new ArrayList<>(); 
+            }
+            curr.add(nums[i]);
+            if(currmax>max){
+                max = currmax;
+                ans = curr;
+            }
+        }
 
-        return n%2==0?hp*hp:(hp*hp*x);
+        for(Integer num : ans){
+            System.out.print(num+"  ");
+        }
+
+        return max;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(pow(1.5, 2));
+        int ans[] = {2, 3, 5, -2, 7, -4};
+        System.out.println(maxSubArray(ans));
     }     
 }  
     
