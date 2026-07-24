73fc1527e280caf9f33d0b75af1871fa5b5f2928
Sachin Kumar <sachinkumar9031735255@gmail.com>
2026-07-24 14:45:09 +0530

    Day 37 - changes

--- sde.py
+++ sde.py
@@ -1,10 +1,3 @@
-from google import genai
 
-client = genai.Client(api_key="AIzaSyB6VWKw7H7eaU8rltDp3Sv0DMaVBmYL7Kg")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Say hello in one sentence"
-)
  
 print(response.text)
